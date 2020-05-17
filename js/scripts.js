57c5f4823713ec765cc84e5f6d4b49bb984619f6
ramzan <ramzanthenolph@gmail.com>
2020-05-17 09:29:10 +0300

    add delivery form

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+//user interface logic
+$(document).ready(function(){
+    $("#deliver").click(function(){
+        $("#deliverydetails").append(
+        '<div class="deliverydetails"'+
+        '<h6>Hi. Fill the delivery</h6>'+
+        '<p>Delivery at sh.150</p>'+
+        '<div class="form-group">' +
+          '<label for="deliveryName">Name</label>' +
+          '<input type="text" class="form-control deliveryName">' +
+        '</div>' +
+        '<div class="form-group">' +
+          '<label for="deliveryNumber">Phone Number</label>' +
+          '<input type="text" class="form-control deliveryNumber">' +
+        '</div>' +
+        '<div class="form-group">' +
+          '<label for="deliveryAddress">Address</label>' +
+          '<input type="text" class="form-control deliveryAddress">' +
+        '</div>' +
+        '<span class="btn btn-outline-info" id="submit">Submit Details</span><br>'+
+        '<span class="btn btn-outline-secondary" id="checkout">Checkout</span>'+
+        '</div>');
+    });
+});
