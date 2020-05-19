6618e94c07ac1b32332c2a1f7133464e94f903e2
ramzan <ramzanthenolph@gmail.com>
2020-05-19 12:07:39 +0300

    add pricing

--- js/scripts.js
+++ js/scripts.js
@@ -33,12 +33,31 @@ $(document).ready(function(){
         '<span class="btn btn-secondary" id="checkout">Checkout</span>'+
         '</div>');
     });
-    $("#submit").click(function(event){
+    $("span#submit").click(function(event){
       event.preventDefault();
       var inputtedName=  $("input.deliveryName").val();
       var inputtedNumber= $("input#deliveryNumber").val();
       var inputtedAddress= $("input#deliveryAddress").val();
       var newDelivery= new Delivery(inputtedName, inputtedNumber, inputtedAddress);
       console.log("Hello "+inputtedName+ ", your delivery has been processed and will take upto 30 mins");    
+    });
+    $("#checkout").click(function(){
+      let size = $("#size option:selected").val();
+      let topping = $("#toppings option:selected").val();
+      let totalPrice, price;
+      topping=50
+
+      switch(size){
+        case size= "small":
+          price=550;
+          totalPrice= price+topping+150;
+        case size= "medium":
+          price= 850;
+          totalPrice= price+topping+150;
+        case size= "large":
+          price= 1200;
+          totalPrice= price+topping+150;
+      }
+      console.log("Your total price is sh." +totalPrice+".")
     });
 });
