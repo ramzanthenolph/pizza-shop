fd484bc00fb05d347f7f37c09e98e7301ca7475c
ramzan <ramzanthenolph@gmail.com>
2020-05-27 11:27:57 +0300

    add delivery details to index

--- js/scripts.js
+++ js/scripts.js
@@ -10,26 +10,7 @@ function Delivery(name, phone, address){
 //user interface logic
 $(document).ready(function(){
     $("#deliver").click(function(){
-        $("#deliverydetails").append(
-        '<div class="deliverydetails"'+
-        '<h6>Hi. Fill the delivery</h6>'+
-        '<p>Delivery at sh.150</p>'+
-        '<div class="form-group">' +
-          '<label for="deliveryName">Name</label>' +
-          '<input type="text" class="form-control deliveryName">' +
-        '</div>' +
-        '<div class="form-group">' +
-          '<label for="deliveryNumber">Phone Number</label>' +
-          '<input type="text" class="form-control deliveryNumber">' +
-        '</div>' +
-        '<div class="form-group">' +
-          '<label for="deliveryAddress">Address</label>' +
-          '<input type="text" class="form-control deliveryAddress">' +
-        '</div>' +
-        '<span class="btn btn-info" id="submit">Submit Details</span><br>'+
-        '<span class="btn btn-secondary" id="checkout">Checkout</span>'+
-        '<p class="total">Total: </p>'+
-        '</div>');
+        
     });
     
     $("#checkout").click(function(){
